9425f277b518fdd4162b7ac11854378392e336cb
Eugene <marat@192-168-1-103.local>
2019-04-10 21:33:24 +0300

    test

--- public/script.js
+++ public/script.js
@@ -11,7 +11,10 @@ const app = new Vue({
     catalogData: []
   },
   computed: {
-    //  
+    // вычисляемые
+    totalCost() {
+      return this.getBasket.reduce((acc, good) => acc + good.price * good.quantity, 0);
+    }
   },
   methods: {
     handleSearchClick() {
@@ -19,7 +22,81 @@ const app = new Vue({
       this.filteredGoods = this.goods.filter((good) => regexp.test(good.name));
       console.log(this.filteredGoods);
     },
+    handleBuyClick(goods) {
+      const basketGood = this.getBasket.find((entry) => entry.id === goods.id);
+      if (basketGood) {
+        // товар есть => увеличим количество
+        fetch(`${API_URL}/getbasket/${goods.id}`, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              quantity: basketGood.quantity + 1
+            }),
+          })
+          // обрабатываем запрос
+          .then((response) => response.json())
+          .then((goods) => {
+            const goodsIdx = this.getBasket.findIndex((entry) => entry.id === goods.id);
+            // тоже рабочий вариант:
+            // this.getBasket[goodsIdx].quantity = goods.quantity;
+            // но тот что ниже => Лучше
+            // vue отслеживает свойства и в массив-getBasket по найденному индексу-goodsIdx заносит данные-goods
+            Vue.set(this.getBasket, goodsIdx, goods);
+          });
+      } else {
+        // товара нет => добавим товар
+        fetch(`${API_URL}/getbasket`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            // далее, в body (ключевое слово) передаем то что отправляем на сервер
+            // здесь интересный момент 24 минута урока номер 6
+            // здесь нужно подумать, когда у товара на складе есть количество !!!
+            body: JSON.stringify({
+              ...goods,
+              quantity: 1
+            })
+          })
+          // обработка ответа от сервера
+          .then((response) => response.json())
+          .then((goods) => {
+            this.getBasket.push(goods);
+          });
+      }
+    },
+    handleDeleteClick(goods) {
+      if (goods.quantity > 1) {
+        fetch(`${API_URL}/getbasket/${goods.id}`, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ quantity: goods.quantity - 1 }),
+          })
+          // обрабатываем запрос
+          .then((response) => response.json())
+          .then((goods) => {
+            const goodsIdx = this.getBasket.findIndex((entry) => entry.id === goods.id);
+            // тоже рабочий вариант:
+            // this.getBasket[goodsIdx].quantity = goods.quantity;
+            // но тот что ниже => Лучше
+            // vue отслеживает свойства и в массив-getBasket по найденному индексу-goodsIdx заносит данные-goods
+            Vue.set(this.getBasket, goodsIdx, goods);
+          });
+      } else {
+        fetch(`${API_URL}/getbasket/${goods.id}`, {
+            method: 'DELETE',
+          })
+          .then(() => {
+            this.getBasket = this.getBasket.filter((basketGood) => basketGood.id !== goods.id);
+          });
+      }
+    }
   },
+
   mounted() {
     fetch(`${API_URL}/getbasket`)
       .then(response => response.json())
